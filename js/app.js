46d468a7f7821235d07570adc1def20fd7c1cb71
shahinul <shahinul37@outlook.com>
2022-02-17 20:16:53 +0600

    code polished

--- js/app.js
+++ js/app.js
@@ -57,31 +57,3 @@ savings.addEventListener("keyup", function (e) {
 
 //target save button
 let saveBtn = document.getElementById("savings-btn");
-
-//calculate expenses
-/* function sum() {
-  let totalExpense =
-    parseFloat(rentCost.value) +
-    parseFloat(foodCost.value) +
-    parseFloat(clothingCost.value);
-  return totalExpense;
-} */
-
-//get remained money after expense
-/* function substract() {
-  let updatedBalance = parseFloat(earning.value) - totalExpense;
-  return updatedBalance;
-} */
-
-//savings amount
-/* function saingsAmount() {
-  let monthlySave =
-    (parseFloat(savings.value) / 100) * parseFloat(earning.value);
-  return monthlySave;
-} */
-
-//remaining balance after all expense and savings
-/* function remainingBalance() {
-  let remainedMoney = updatedBalance - monthlySave;
-  return remainedMoney;
-} */
