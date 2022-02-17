1d87266f9b2586f8c03d04684e2a2617729004db
shahinul <shahinul37@outlook.com>
2022-02-17 19:43:07 +0600

    final remaining balance done

--- js/savings.js
+++ js/savings.js
@@ -1,5 +1,12 @@
 saveBtn.addEventListener("click", function () {
   let monthlySave =
     (parseFloat(savings.value) / 100) * parseFloat(earning.value);
+  let finalRemainedMoney =
+    parseFloat(earning.value) -
+    (monthlySave +
+      parseFloat(rentCost.value) +
+      parseFloat(foodCost.value) +
+      parseFloat(clothingCost.value));
   document.getElementById("savingAmount").innerText = monthlySave;
+  document.getElementById("finalBalance").innerText = finalRemainedMoney;
 });
